37a282fa26082e9ed79035ae8d21d2bd317b038a
Thais Tozatto <thais.c.tozatto@gmail.com>
2023-01-23 21:05:47 -0300

    Buttons actions

--- app/src/main/java/br/com/ttozatto/m1tp/ui/additems/AddItemActivity.kt
+++ app/src/main/java/br/com/ttozatto/m1tp/ui/additems/AddItemActivity.kt
@@ -2,6 +2,9 @@ package br.com.ttozatto.m1tp.ui.additems
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import br.com.ttozatto.m1tp.R
 import br.com.ttozatto.m1tp.databinding.ActivityAddItemBinding
 import br.com.ttozatto.m1tp.databinding.ActivityMainBinding
 
@@ -14,5 +17,45 @@ class AddItemActivity : AppCompatActivity() {
         binding = ActivityAddItemBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        configureListeners()
+    }
+
+    private fun configureListeners() {
+        binding.btnAddItem.setOnClickListener {
+            addItem()
+        }
+    }
+
+    private fun addItem() {
+        if (!validateItem())
+            return;
+
+        Toast.makeText(
+            applicationContext,
+            "${binding.etProductName.text.toString()} - ${binding.etQuantity.text.toString()}",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        finish()
+    }
+
+    private fun validateItem() : Boolean {
+        var result = true
+
+        binding.apply {
+            if (etProductName.text.isNullOrEmpty()) {
+                tilProductName.error = getString(R.string.invalid_product)
+                result = false
+            } else
+                tilProductName.error = null;
+
+            if (etQuantity.text.isNullOrEmpty() || Integer.parseInt(etQuantity.text.toString()) <= 0) {
+                tilQuantity.error = getString(R.string.invalid_quantity)
+                result = false
+            } else
+                tilQuantity.error = null;
+        }
+
+        return result
     }
 }
--- app/src/main/java/br/com/ttozatto/m1tp/ui/main/MainActivity.kt
+++ app/src/main/java/br/com/ttozatto/m1tp/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package br.com.ttozatto.m1tp.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.ttozatto.m1tp.R
 import br.com.ttozatto.m1tp.databinding.ActivityMainBinding
+import br.com.ttozatto.m1tp.ui.additems.AddItemActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        configureListeners()
+    }
+
+    private fun configureListeners() {
+        binding.fabNewItem.setOnClickListener{
+            openActivityNewItem()
+        }
+    }
+
+    private fun openActivityNewItem() {
+        startActivity(Intent(this, AddItemActivity::class.java))
     }
 }
